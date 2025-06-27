93d826603c566431d85bde72df5a6b15273ebc10
MantillaSergio <mantilla_sergio@hotmail.com>
2025-06-26 21:37:12 -0500

    Delete src directory

--- src/main.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-const App = () => <h1>TopFood Cliente</h1>;
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
